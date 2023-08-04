739b96acc560472f8f981fcf43c7fcc90c6afff7
gilgeunwoo <kesksjxhx@gmail.com>
2023-08-04 23:37:20 +0900

    🧩 :: request 값받기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
 import { AppService } from './app.service';
+
+export class HelloDto {
+  hello: string
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +12,20 @@ export class AppController {
   @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/hello')
+  postBody(@Body() helloDto: HelloDto): HelloDto {
+    return helloDto;
+  }
+
+  @Get('/:userId')
+  getParam(@Param('userId') userId: string): string {
+    return userId;
+  }
+
+  @Get()
+  getQuery(@Query('hello') hello: string): string {
+    return hello;
   }
 }
