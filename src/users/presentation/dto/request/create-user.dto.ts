dc1b5839d6b610b51f64d128b1be06bd0d3c1caf
gilgeunwoo <kesksjxhx@gmail.com>
2023-08-11 00:36:20 +0900

    🧩 :: 유효성 검사

--- src/users/presentation/dto/request/create-user.dto.ts
+++ src/users/presentation/dto/request/create-user.dto.ts
@@ -1,5 +1,13 @@
+import {IsEmail, IsStrongPassword, MaxLength, MinLength} from "class-validator";
+
 export class CreateUserDto {
+
+    @MinLength(1)
+    @MaxLength(20)
     readonly name: string;
+
+    @IsEmail()
     readonly email: string;
+
     readonly password: string;
 }
